26fd1e0da7ba08bdf16f7edc33978ae97d818409
Jarek Krochmalski <jotka@users.noreply.github.com>
2023-06-08 09:00:30 +0530

    fixes

--- src/test/java/com/example/javamavenjunithelloworld/CalculatorTest.java
+++ src/test/java/com/example/javamavenjunithelloworld/CalculatorTest.java
@@ -21,7 +21,7 @@ public class CalculatorTest {
         int b = 2;
         int sum = calculator.add(a, b);
 
-        assertThat("sum", sum == 3);
+        assertThat("sum", sum == 4);
     }
 
     @Test
